afca137b017dc1eb32a857eff6455ebd8be8cc74
ddkhoa <hi.ddkhoa5797@gmail.com>
2021-09-27 11:34:57 +0700

    Add morgan package and edit README.md

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'src/app.module';
+import * as logger from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   const port = 3000;
+  app.use(logger('dev'));
   await app.listen(port, () =>
     console.info('Server ready at: http://localhost:%d', port),
   );
